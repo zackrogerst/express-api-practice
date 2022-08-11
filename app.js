3184fb280aba4b751bebd2d6a38eab8373fa84cd
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-11 16:57:42 -0600

    added api get for specic id in url param

--- app.js
+++ app.js
@@ -27,6 +27,26 @@ app.get("/api/v1/tours", (req, res) => {
 	});
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+	const id = +req.params.id;
+
+	const tour = tours.find(el => el.id === id);
+
+	if (!tour) {
+		return res.status(404).json({
+			status: "fail",
+			message: "Invalid ID"
+		});
+	} else {
+		res.status(200).json({
+			status: "success",
+			data: {
+				tour
+			}
+		});
+	}
+});
+
 app.post("/api/v1/tours", (req, res) => {
 	// console.log(req.body);
 
