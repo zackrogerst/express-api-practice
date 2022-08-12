cc70618a7084b417e96f545d455d049a27a47177
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-11 18:40:57 -0600

    added server file, added start script

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-/////////////////////////////////////// start server
+/////////////////////////////////////// export app
 
-const port = 3000;
-app.listen(port, () => console.log(`App running on port ${port}`));
+module.exports = app;
